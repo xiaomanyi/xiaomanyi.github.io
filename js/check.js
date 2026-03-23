56b9d27017998674f4a8e01b0dacc169c1689db5
Kevin Chen <49828153+xiaomanyi@users.noreply.github.com>
2026-03-23 15:14:07 +0800

    first commit

---
+++ js/check.js
@@ -0,0 +1,8 @@
+// (function checkLogin() {
+//     const loginToken = sessionStorage.getItem('loginToken');
+//     if (!loginToken) {
+//         window.location.href = '../index.html';
+//     }
+// })();
+
+(function checkLogin(){const a=sessionStorage.getItem("loginToken");if(!a){window.location.href="../index.html"}})();
