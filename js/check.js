bc2c751a7fe82bf6051d69fdbaf6aae62eadbc4b
Kevin Chen <49828153+xiaomanyi@users.noreply.github.com>
2026-03-24 12:43:24 +0800

    new verson

--- js/check.js
+++ js/check.js
@@ -1,8 +1,1 @@
-// (function checkLogin() {
-//     const loginToken = sessionStorage.getItem('loginToken');
-//     if (!loginToken) {
-//         window.location.href = '../index.html';
-//     }
-// })();
-
 (function checkLogin(){const a=sessionStorage.getItem("loginToken");if(!a){window.location.href="../index.html"}})();
